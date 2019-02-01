ed5932bb24027a1c65329124cca790319ee403bc
Nathaniel Wesley Filardo <nwfilardo@gmail.com>
2019-02-01 23:43:26 +0000

    Support more recent OpenSSL rsa -text formats

--- acme_tiny.py
+++ acme_tiny.py
@@ -70,7 +70,7 @@ def get_crt(account_key, csr, acme_dir, log=LOGGER, disable_check=False, directo
     # parse account key to get public key
     log.info("Parsing account key...")
     out = _cmd(["openssl", "rsa", "-in", account_key, "-noout", "-text"], err_msg="OpenSSL Error")
-    pub_pattern = r"modulus:\n\s+00:([a-f0-9\:\s]+?)\npublicExponent: ([0-9]+)"
+    pub_pattern = r"[Mm]odulus:\n\s+00:([a-f0-9\:\s]+?)\n(?:public)Exponent: ([0-9]+)"
     pub_hex, pub_exp = re.search(pub_pattern, out.decode('utf8'), re.MULTILINE|re.DOTALL).groups()
     pub_exp = "{0:x}".format(int(pub_exp))
     pub_exp = "0{0}".format(pub_exp) if len(pub_exp) % 2 else pub_exp
